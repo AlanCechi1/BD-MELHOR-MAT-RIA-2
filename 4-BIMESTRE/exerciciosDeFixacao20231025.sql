2d3aaea8789876dcde129503aee75e125493356c
AlanCechi1 <116587479+AlanCechi1@users.noreply.github.com>
2023-10-25 21:42:49 -0300

    segundo-commit

--- 4-BIMESTRE/exerciciosDeFixacao20231025.sql
+++ 4-BIMESTRE/exerciciosDeFixacao20231025.sql
@@ -9,3 +9,15 @@ BEGIN
 END;
 //
 DELIMITER ;
+
+Trigger para tentativa de exclusão de um cliente da tabela Clientes:
+
+DELIMITER //
+CREATE TRIGGER cliente_delete_trigger
+BEFORE DELETE ON Clientes
+FOR EACH ROW
+BEGIN
+    INSERT INTO Auditoria (mensagem) VALUES (CONCAT('Tentativa de exclusão de cliente com ID ', OLD.id, ' em ', NOW()));
+END;
+//
+DELIMITER ;
